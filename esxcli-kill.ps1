cd8449a74cf2a03ec0ef6dce320954c683895cfc
ozgurmazlum <ozgurmazlum@outlook.com>
2018-12-30 19:17:24 +0300

    esxcli kill

---
+++ esxcli-kill.ps1
@@ -0,0 +1,21 @@
+﻿# Powershell Gallery PowerCLI modullerini yukluyoyuruz
+Import-Module VMware.PowerCLI
+
+
+Connect-VIServer -Server vcenterip
+
+$vName =  Read-Host "Vm Adını Yazın"
+
+$vmName = Get-VM -name $vName
+
+$esxcli = Get-EsxCli -Server $($vmName.VMHost.Name)
+
+$esxcli.vm.process.list()
+
+$vmWorldID = ($esxcli.vm.process.list() | Select-Object -Property DisplayName, WorldID | Where-Object -Property DisplayName -EQ "$vmName").WorldID
+
+$esxcli.vm.process.kill("force","$vmWorldID")
+
+
+Disconnect-VIServer * -Confirm:$false
+
