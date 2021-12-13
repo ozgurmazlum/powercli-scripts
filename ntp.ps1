6946abe8dea1a7f4cbdb8db9d2e7c8c03568d1fd
Ozgur Mazlum <ozgurmazlum@outlook.com>
2021-12-13 11:05:58 +0300

    Create ntp.ps1

---
+++ ntp.ps1
@@ -0,0 +1,46 @@
+Import-Module -name VMware.PowerCLI -ErrorAction Stop
+
+
+Connect-VIServer -server "vcenter.yerelag.local"-username "administrator@vsphere.local" -password "Vmware01!!"
+
+Get-VMHost | Get-VmHostService | Where-Object {$_.key-eq "ntpd"}
+
+Get-VMHost | Add-VMHostNtpServer -NtpServer pool.ntp.org
+Get-VMHost | Sort-Object Name | Select-Object Name, @{N=”Cluster”;E={$_ | Get-Cluster}}, @{N=”Datacenter”;E={$_ | Get-Datacenter}}, @{N=“NTPServiceRunning“;E={($_ | Get-VmHostService | Where-Object {$_.key-eq “ntpd“}).Running}}, @{N=“StartupPolicy“;E={($_ | Get-VmHostService | Where-Object {$_.key-eq “ntpd“}).Policy}}, @{N=“NTPServers“;E={$_ | Get-VMHostNtpServer}}, @{N="Date&Time";E={(get-view $_.ExtensionData.configManager.DateTimeSystem).QueryDateTime()}} | format-table -autosize
+
+
+
+$currentNtp = "tr.pool.ntp.org"
+
+$xVmHosts = Get-VMHost | Sort-Object Name
+
+foreach ($xVmHost in $xVmHosts) {
+
+    $xNtpServers = $xVmHost | Get-VMHostNtpServer
+
+    if ($xNtpServers.Count -eq 0) {
+
+        $xVmHost | Add-VMHostNtpServer -NtpServer $currentNtp
+
+    } else {
+
+        $xNtpServers | ForEach-Object {
+
+            if ($_.NtpServer -eq $currentNtp) {
+
+                $_.Remove() 
+
+            }
+
+        }
+
+        $xVmHost | Add-VMHostNtpServer -NtpServer $currentNtp
+
+    }
+
+
+}
+
+
+
+Disconnect-VIServer -Server * -Confirm:$false
